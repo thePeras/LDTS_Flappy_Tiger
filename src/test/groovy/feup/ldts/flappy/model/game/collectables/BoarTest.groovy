fd76e78eaf2baabfe0af38e2e4848ac31cf5ab6e
thePeras <jtopereira@gmail.com>
2022-12-17 12:10:02 +0000

    Player, Ground and Position tests

---
+++ src/test/groovy/feup/ldts/flappy/model/game/collectables/BoarTest.groovy
@@ -0,0 +1,7 @@
+package feup.ldts.flappy.model.game.collectables
+
+import spock.lang.Specification
+
+class BoarTest extends Specification{
+
+}
---
+++ src/test/groovy/feup/ldts/flappy/model/game/collectables/PortoWineTest.groovy
@@ -0,0 +1,6 @@
+package feup.ldts.flappy.model.game.collectables
+
+import spock.lang.Specification
+
+class PortoWineTest extends Specification{
+}
